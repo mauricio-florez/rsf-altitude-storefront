4ed757d2671b996da87e473430d45046028b69f9
Mauricio Florez <mauricio.florez@altitude-spots.com>
2022-08-24 15:29:36 -0500

    Support multilingual request and retrieve right resources

--- localPackages/altitude-commercetools-connector/src/category.js
+++ localPackages/altitude-commercetools-connector/src/category.js
@@ -1,6 +1,7 @@
 import createAppData from './clients/commercetools-v2/utils/createAppData'
 import fulfillAPIRequest from 'react-storefront/props/fulfillAPIRequest'
 import { getProductsByCategoryId, getCategoryById } from "./clients/commercetools-v2/commercetoolsClient"
+import { getLocale } from './clients/utils/index.ts'
 
 export default async function category(params, req, res) {
   return await fulfillAPIRequest(req, {
@@ -10,24 +11,24 @@ export default async function category(params, req, res) {
 }
 
 async function getPageData(params, req) {
-  const lang = "en-CA"
+  const lang = getLocale({host: req.headers.host})
   const { filters = '[]' } = params
   const { categorySlug } = req.query
   const { body: category } = await getCategoryById({ categoryId: categorySlug[0] })
   const plp = await getProductsByCategoryId({ categoryId: categorySlug[0] })
-
+  const categoryName = category.name[lang]
   // collect all page data
   return {
       id: category.id,
-      name: category.name[lang],
-      title:category.name[lang],
+      name: categoryName,
+      title: categoryName,
       breadcrumbs: 
       [{
               text: 'Home',
               href: '/',
           },
           {
-              text: category.name[lang],
+              text: categoryName,
               href: `/s/${category.id}`,
           },
       ],
