3b2032bd77b4449d4812b722d904704de4a2f4c4
Mauricio Florez <mauricio.florez@altitude-spots.com>
2022-08-24 15:26:49 -0500

    Update app data - menu generation in ctconnector v2

--- localPackages/altitude-commercetools-connector/src/clients/commercetools-v2/utils/createAppData.js
+++ localPackages/altitude-commercetools-connector/src/clients/commercetools-v2/utils/createAppData.js
@@ -1,37 +1,7 @@
-import getClient from '../../commercetools/commercetools-client'
-// TODO move create data to v-2
-export default async function createAppData(req) {
-    const client = await getClient(req)
-    const { results = [] } = await client.getMenu();  
- 
-    const data = results.filter(cat => cat.ancestors.length != 0)
-    const finalresults = data.filter(cat => cat.ancestors.length == 1)
-        .map(cat => {
-            return {
-                id: cat.id,
-                name: cat.name.en,
-                childrens: data.filter(subcat => subcat.parent.id === cat.id && cat.ancestors.length != 0)
-                    .map(subcat => { return { id: subcat.id, name: subcat.name.en } })
-            }
-        })
-    
-    const tabs = finalresults.map(cat => {
-        const tab = {
-            text: cat.name,
-            as: `/s/${cat.id}`,
-            href: '/s/[...categorySlug]',
-        }
-        if (cat.childrens) {
-            tab.items = cat.childrens.map(cat => {
-                return {
-                    text: cat.name,
-                    as: `/s/${cat.id}`,
-                    href: '/s/[...categorySlug]',
-                }
-            })
-        }
-        return tab
-    })
+import getContentFulClient from '../../contentful/contentful-client'
 
-    return Promise.resolve({ menu: { items: tabs }, tabs })
+export default async function createAppData(req) {
+    const categoryTree = await getContentFulClient().getCategoryTree();
+
+    return Promise.resolve({ categoryTree })
 }
