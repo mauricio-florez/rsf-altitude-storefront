8544de808d69e1a886f82ef400aa72c20bb5afe4
Mauricio Florez <mauricio.florez@altitude-spots.com>
2022-08-24 15:24:30 -0500

    Filter category by id

--- localPackages/altitude-commercetools-connector/src/clients/commercetools-v2/commercetoolsClient.ts
+++ localPackages/altitude-commercetools-connector/src/clients/commercetools-v2/commercetoolsClient.ts
@@ -14,10 +14,11 @@ const getProductsByCategoryId = async ({ req= {}, params= {}, categoryId='' }): 
     const { body: search } = await apiRoot
       .withProjectKey({ projectKey })
       .productProjections()
+      .search()
       .get({
         queryArgs: {
-          facet: [],
-          filter: [`categories.id:${categoryId}`],
+          filter: [`categories.id:"${categoryId}"`],
+          facet: ['variants.attributes.vendorTitle'],
           "filter.query": []
         }
       })
