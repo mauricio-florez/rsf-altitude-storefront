62b32f4e05a0dda8773553cb396672631bf39882
Alejandro Sobko <alejandro.sobko@gmail.com>
2022-09-05 08:22:30 -0300

    use named props

--- localPackages/altitude-commercetools-connector/src/category.js
+++ localPackages/altitude-commercetools-connector/src/category.js
@@ -20,7 +20,7 @@ async function getPageData({ params, req, locale }) {
   const filterQuery = (filterKey && `${filterKey}:${req.query[filterKey]}`) || ''
   const { categorySlug } = req.query
 
-  const facets = await getContentFulClient().getFacets(locale)
+  const facets = await getContentFulClient().getFacets({locale})
   const { body: category } = await getCategoryById({ categoryId: categorySlug[0] })
   const plp = await getProductsByCategoryId({ categoryId: categorySlug[0], facets, filterQuery })
 
